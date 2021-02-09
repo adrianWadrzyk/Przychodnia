25887ff660ce622e4038a24ab610084c1c920cec
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-02-09 21:05:37 +0100

    add full register patient and more advanced valid

--- Przychodnia/RegisterWindow.xaml.cs
+++ Przychodnia/RegisterWindow.xaml.cs
@@ -27,23 +27,31 @@ namespace Przychodnia
 
         private void add_patient(object sender, RoutedEventArgs e)
         {
-            pacjent pacjent = new pacjent()
+         
+            if(!registerDate.SelectedDate.HasValue)
             {
-                imie = name_box.Text, 
-                nazwisko = surname_box.Text, 
-                PESEL = pesel_box.Text, 
-                telefon = phone_box.Text,
-                miasto = city_box.Text,
-                ulica = street_box.Text,
-                nr_lokalu = house_num_box.Text
-            };
-            
+                MessageBox.Show("Wybierz datę wizyty!");
+                return;
+            }
+            if (pesel_box.Text.Length != 11)
+            {
+                MessageBox.Show("Pesel musi zawierać 11 liczb!");
+                return;
+            }
+            if (phone_box.Text.Length != 9)
+            {
+                MessageBox.Show("Telefon musi zawierać 9 liczb");
+                return;
+            }
+            if (doctors.SelectedIndex == -1)
+            {
+                MessageBox.Show("Musisz wybrać doktora prowadzącego!");
+                return;
+            }
+
             try
             {
-                if (pesel_box.Text.Length == 11)
-                    int.Parse(pesel_box.Text);
-                else
-                    MessageBox.Show("Pesel musi zawierać 11 liczb!");
+               decimal.Parse(pesel_box.Text);
             } catch
             {
                 MessageBox.Show("Pesel nie może zawierać liter");
@@ -52,17 +60,44 @@ namespace Przychodnia
 
             try
             {
-                if (phone_box.Text.Length == 9)
-                    int.Parse(phone_box.Text);
-                else
-                    MessageBox.Show("Telefon musi zawierać 9 liczb");
-            } catch
+                decimal.Parse(phone_box.Text);
+            }
+            catch
             {
                 MessageBox.Show("Telefon nie może zawierać liter");
                 return;
             }
+
+            pacjent pacjent = new pacjent()
+            {
+                imie = name_box.Text,
+                nazwisko = surname_box.Text,
+                PESEL = pesel_box.Text,
+                telefon = phone_box.Text,
+                miasto = city_box.Text,
+                ulica = street_box.Text,
+                nr_lokalu = house_num_box.Text
+            };
+
+            rejestracja rejestration = new rejestracja()
+            {
+                id_pacjenta = pacjent.id_pacjenta,
+                id_lekarza = doctors.SelectedIndex+1,
+                data_rejestracji = registerDate.SelectedDate.Value,
+                godzina = new TimeSpan(10, 00, 11)
+            };
+
+            try
+            {
                 context.pacjent.Add(pacjent);
-                context.SaveChanges();
+                context.rejestracja.Add(rejestration);
+            }catch
+            {
+                MessageBox.Show("Ups! Coś poszło nie tak!");
+                return;
+            }
+                
+             context.SaveChanges();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -70,6 +105,19 @@ namespace Przychodnia
             MainWindow main = new MainWindow();
             this.Hide();
             main.Show();
+        }
+
+        private void getDoctors()
+        {
+            var results = from row in context.lekarz select row;
+            foreach(var x in results)
+                doctors.Items.Add(x.imie);
+            MessageBox.Show("Work");
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.getDoctors();
         }
     }
 }
